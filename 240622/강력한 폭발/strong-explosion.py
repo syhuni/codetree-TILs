1b0fc5b6ba017809637433a7c3360c057d6dd2eb
syhuni <74291750+syhuni@users.noreply.github.com>
2024-06-22 12:15:36 +0900

    Title: 강력한 폭발, Time: 5084ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240622/강력한 폭발/strong-explosion.py
+++ 240622/강력한 폭발/strong-explosion.py
@@ -16,31 +16,40 @@ def bomb(def_cnt):
     global loc_cnt
     global max_val
     if def_cnt == loc_cnt:
-        max_val = max(max_val, loc_counter(n, bomb_grid))
+        max_val = max(max_val, bomb_counter(n, bomb_grid))
         return
 
     for k in range(1, 4):  # 폭탄 종류 1~3
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    # 폭발 구역 체크
-                    bomb_grid[i][j] += 1
-                    for di, dj in didj_dict.get(k):
-                        ni, nj = i + di, j + dj
-                        if 0 <= ni < n and 0 <= nj < n:
-                            bomb_grid[ni][nj] += 1
-                    # 함수 실행
-                    bomb(def_cnt + 1)
-                    # 폭발 구역 원복
-                    bomb_grid[i][j] -= 1
-                    for di, dj in didj_dict.get(k):
-                        ni, nj = i + di, j + dj
-                        if 0 <= ni < n and 0 <= nj < n:
-                            bomb_grid[ni][nj] -= 1
+        for i, j in loc_lst:
+            # 폭발 구역 체크
+            bomb_grid[i][j] += 1
+            for di, dj in didj_dict.get(k):
+                ni, nj = i + di, j + dj
+                if 0 <= ni < n and 0 <= nj < n:
+                    bomb_grid[ni][nj] += 1
+            # 함수 실행
+            bomb(def_cnt + 1)
+            # 폭발 구역 원복
+            bomb_grid[i][j] -= 1
+            for di, dj in didj_dict.get(k):
+                ni, nj = i + di, j + dj
+                if 0 <= ni < n and 0 <= nj < n:
+                    bomb_grid[ni][nj] -= 1
 
 
-# 폭탄 놓을 위치 수와 폭발 영역 위치 수를 세는 함수
+# 폭탄 놓을 위치 수를 세고 위치를 저장 하는 함수
 def loc_counter(n, data):
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if data[i][j] == 1:
+                cnt += 1
+                loc_lst.append((i, j))
+    return cnt
+
+
+# 폭발 개수 세는 함수
+def bomb_counter(n, data):
     cnt = 0
     for i in range(n):
         for j in range(n):
@@ -52,6 +61,7 @@ def loc_counter(n, data):
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 bomb_grid = [[0] * n for _ in range(n)]
+loc_lst = []  # 설치 위치 저장 리스트
 loc_cnt = loc_counter(n, grid)
 max_val = 0
 bomb(0)
