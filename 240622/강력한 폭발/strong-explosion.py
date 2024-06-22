622ec70ca1ee68723336dc2822aef1ffa973bbd8
syhuni <74291750+syhuni@users.noreply.github.com>
2024-06-22 12:20:25 +0900

    Title: 강력한 폭발, Time: 5091ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240622/강력한 폭발/strong-explosion.py
+++ 240622/강력한 폭발/strong-explosion.py
@@ -15,26 +15,35 @@ didj_dict = {
 def bomb(def_cnt):
     global loc_cnt
     global max_val
+    global bomb_cnt
     if def_cnt == loc_cnt:
-        max_val = max(max_val, bomb_counter(n, bomb_grid))
+        max_val = max(max_val, bomb_cnt)
         return
 
     for k in range(1, 4):  # 폭탄 종류 1~3
         for i, j in loc_lst:
             # 폭발 구역 체크
             bomb_grid[i][j] += 1
+            if bomb_grid[i][j] == 1:
+                bomb_cnt += 1
             for di, dj in didj_dict.get(k):
                 ni, nj = i + di, j + dj
                 if 0 <= ni < n and 0 <= nj < n:
                     bomb_grid[ni][nj] += 1
+                    if bomb_grid[ni][nj] == 1:
+                        bomb_cnt += 1
             # 함수 실행
             bomb(def_cnt + 1)
             # 폭발 구역 원복
             bomb_grid[i][j] -= 1
+            if bomb_grid[i][j] == 0:
+                bomb_cnt -= 1
             for di, dj in didj_dict.get(k):
                 ni, nj = i + di, j + dj
                 if 0 <= ni < n and 0 <= nj < n:
                     bomb_grid[ni][nj] -= 1
+                    if bomb_grid[ni][nj] == 0:
+                        bomb_cnt -= 1
 
 
 # 폭탄 놓을 위치 수를 세고 위치를 저장 하는 함수
@@ -46,17 +55,7 @@ def loc_counter(n, data):
                 cnt += 1
                 loc_lst.append((i, j))
     return cnt
-
-
-# 폭발 개수 세는 함수
-def bomb_counter(n, data):
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if data[i][j] >= 1:
-                cnt += 1
-    return cnt
-
+    
 
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
@@ -64,5 +63,6 @@ bomb_grid = [[0] * n for _ in range(n)]
 loc_lst = []  # 설치 위치 저장 리스트
 loc_cnt = loc_counter(n, grid)
 max_val = 0
+bomb_cnt = 0
 bomb(0)
 print(max_val)
