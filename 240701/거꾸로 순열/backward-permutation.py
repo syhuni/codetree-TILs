f645e0e1978fabfc1d748486c27114d42c2f6526
syhuni <74291750+syhuni@users.noreply.github.com>
2024-07-01 16:25:34 +0900

    Title: 거꾸로 순열, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240701/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,22 @@
+n = int(input())
+perm =[]
+visited = [0] * (n + 1)
+
+def dfs(cnt):
+    if cnt == 3:
+        print(' '.join(map(str, perm)))
+        return
+
+    for i in range(n, 0, -1):
+        if visited[i]:
+            continue
+        
+        perm.append(i)
+        visited[i] = 1
+
+        dfs(cnt + 1)
+
+        perm.pop()
+        visited[i] = 0
+
+dfs(0)
